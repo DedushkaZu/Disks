f2ac538d4a94c2d653172b71d0d1a62a706d7c88
bzelijah <mayzzze@gmail.com>
2021-03-04 11:13:13 +0300

    env

--- server/App.js
+++ server/App.js
@@ -1,20 +1,24 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
 const { connect } = require('./Db/Models');
 
-
 const app = express();
+
+const PORT = process.env.PORT ?? 3001;
+const { DB_NAME } = process.env.DB_NAME;
+const { DB_PASSWORD } = process.env.DB_PASSWORD;
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log('server has been started');
-  connect('mongodb://localhost:27017/myGame', {
+  connect(`mongodb+srv://${DB_NAME}:${DB_PASSWORD}@discs.v8j1k.mongodb.net/test`, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
