436b2dd0626b010ef9aa2db9ffc170591c55d2a4
bzelijah <mayzzze@gmail.com>
2021-03-05 19:46:03 +0300

    new photo

--- client/src/components/Configurator/Configurator.js
+++ client/src/components/Configurator/Configurator.js
@@ -8,7 +8,7 @@ function Configurator() {
 
   return (
     <>
-      <div className="cloudimage-360" data-folder="./images/m-d1/" data-filename="m-d1-{index}.jpg" data-amount="12"
+      <div className="cloudimage-360" data-folder="./images/g-d1/" data-filename="g-d1-{index}.jpg" data-amount="20"
       >
       </div>
     </>
--- client/src/components/WelcomePage/WelcomePage.js
+++ client/src/components/WelcomePage/WelcomePage.js
@@ -1,7 +1,15 @@
 const WelcomePage = () => {
   return (
     <div>
-
+      {/* <div>
+        <h4>Name</h4>
+      </div>
+      <div>
+        <h6>Configurators</h6>
+        <div>
+          photos
+        </div>
+      </div> */}
     </div>
   );
 }
