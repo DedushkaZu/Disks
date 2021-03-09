23d6558723b7ece96b387b3d1079c38d5fe23c67
Alexander <kechko02@gmail.com>
2021-03-09 15:34:19 +0300

    Add names disks

--- client/src/components/Configurator/Configurator.js
+++ client/src/components/Configurator/Configurator.js
@@ -20,6 +20,7 @@ function Configurator() {
   const disks = cars[brand][model].disks;
   const smallDisks = cars[brand][model].linkDisksSmall;
   const bigDisks = cars[brand][model].linkDisksBig;
+  const namesDisks = cars[brand][model].nameDisks;
 
 
   const handlerTapacStyle = () => {
@@ -54,7 +55,7 @@ function Configurator() {
     }));
   };
 
-  function handlerDisk(value, linkImageDisk) {
+  function handlerDisk(value, linkImageDisk, nameDisk) {
     setDisk(`d${value}`);
   }
 
@@ -94,7 +95,7 @@ function Configurator() {
               {
                 disks?.length ? (disks.map((el, index) => (
                   <div key={index}>
-                    <div onClick={() => handlerDisk(index + 1, bigDisks[index])} className="disk sm-blocks-d"><img src={smallDisks[index]} alt="disk"></img></div>
+                    <div onClick={() => handlerDisk(index + 1, bigDisks[index], namesDisks[index])} className="disk sm-blocks-d"><img src={smallDisks[index]} alt="disk"></img></div>
                   </div>
                 ))) : <div></div>
               }
--- client/src/redux/initState.js
+++ client/src/redux/initState.js
@@ -19,6 +19,7 @@ const initState = {
         disks: ['d1', 'd2', 'd3'],
         linkDisksSmall: ['', 'discs/g-d2-DEZENT-TA-SILVER-small.png', 'discs/g-d3-DOTZ-REVVO-DARK-small.png'],
         linkDisksBig: ['', 'discs/g-d2-DEZENT-TA-SILVER-big.png', 'discs/g-d3-DOTZ-REVVO-DARK-big.png'],
+        nameDisks: ['', 'DEZENT-TA-SILVER', 'DOTZ-REVVO-DARK'],
         color: ['g', 'b', 'r'],
         photoCount: 24
       }
